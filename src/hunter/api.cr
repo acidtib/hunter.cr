d41dc35b1a21a65c200332f4f9cc9b96242d5eae
arubinofaux <arubinofaux@gmail.com>
2019-11-29 17:38:18 -0500

    wip

--- src/hunter/api.cr
+++ src/hunter/api.cr
@@ -7,17 +7,29 @@ module Hunter
     end
 
     # 200 - OK / The request was successful
+    #
     # 201 - Created / The request was successful and the resource was created
+    #
     # 202 - Email verification in progress / The Email verification is still in progress. Feel free to make the same verification again as often as you want, it will only count as a single request until we return the response
+    #
     # 204 - No content / The request was successful and no additional content was sent
+    #
     # 222 - Email verification failed / The Email verification failed because of an unexpected response from the remote SMTP server. This failure is outside of our control. We advise you to retry later
+    #
     # 400 - Bad request / Your request was not valid
+    #
     # 401 - Unauthorized / No valid API key was provided
+    #
     # 403 - Forbidden / You have reached the rate limit
+    #
     # 404 - Not found / The requested resource does not exist
+    #
     # 422 - Unprocessable entity / Your request is valid but the creation of the resource failed. Check the errors
+    #
     # 429 - Too many requests / You have reached your usage limit. Upgrade your plan if necessary
+    #
     # 451 - Unavailable for legal reasons / The person behind the requested resource has asked us directly or indirectly to stop the processing of this resource. For this reason, you shouldn't process this resource yourself in any way
+    #
     # 5XX - Server errors / Something went wrong on Hunter's end
     def handleResponse(response : HTTP::Client::Response) : JSON::Any
       case response.status_code
