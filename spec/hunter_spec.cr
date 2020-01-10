a44fdb5905f753f7621f84e35effd33c6c117dba
arubinofaux <arubinofaux@gmail.com>
2020-01-09 21:56:40 -0500

    added test and mocked endpoints

--- spec/hunter_spec.cr
+++ spec/hunter_spec.cr
@@ -1,9 +1,35 @@
 require "./spec_helper"
 
 describe Hunter do
-  # TODO: Write tests
+  describe "Client" do
+    it "set up" do
+      CLIENT.api_key.should eq(APIKEY)
+    end
 
-  it "works" do
-    false.should eq(true)
+    it "domain search" do
+      CLIENT.domain.search(domain: "intercom.io")["data"]["domain"].should eq("intercom.io")
+    end
+
+    describe "email" do
+      it "finder" do
+        CLIENT.email.finder(domain: "asana.com", first_name: "Dustin", last_name: "Moskovitz")["data"]["domain"].should eq("asana.com")
+      end
+
+      it "verifier" do
+        CLIENT.email.verifier(email: "steli@close.io")["data"]["email"].should eq("steli@close.io")
+      end
+
+      it "count" do
+        CLIENT.email.count(domain: "intercom.io")["data"]["total"].should eq(46)
+      end
+    end
+
+    it "account" do
+      CLIENT.account["data"]["plan_name"].should eq("Free")
+    end
+
+    it "status" do
+      CLIENT.status["hostname"].should eq("status.hunter.io")
+    end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,21 @@
 require "spec"
+require "webmock"
+require "./support/stub"
 require "../src/hunter"
+
+
+APIKEY = "api_key"
+
+WebMock.allow_net_connect = false
+
+WebMock.stub(:get, "https://api.hunter.io/v2/domain-search?domain=intercom.io&api_key=#{APIKEY}").to_return(body: Stub::DOMAIN_SEARCH)
+
+WebMock.stub(:get, "https://api.hunter.io/v2/email-finder?domain=asana.com&first_name=Dustin&last_name=Moskovitz&api_key=#{APIKEY}").to_return(body: Stub::Email::FINDER)
+WebMock.stub(:get, "https://api.hunter.io/v2/email-verifier?email=steli%40close.io&api_key=#{APIKEY}").to_return(body: Stub::Email::VERIFIER)
+WebMock.stub(:get, "https://api.hunter.io/v2/email-count?domain=intercom.io").to_return(body: Stub::Email::COUNT)
+
+WebMock.stub(:get, "https://api.hunter.io/v2/account?api_key=#{APIKEY}").to_return(body: Stub::ACCOUNT)
+
+WebMock.stub(:get, "https://status.hunter.io/api/v1/public/services/status.hunter.io").to_return(body: Stub::STATUS)
+
+CLIENT = Hunter.new(APIKEY)
